4e81d44e16c84921dff045f0c6ccbcda8ac63ea9
saepulmilah06 <93575403+saepulmilah06@users.noreply.github.com>
2022-02-08 13:07:40 +0700

    Add files via upload

---
+++ src/components/navigasi.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav class="nav navbar-light" style="background-color: #e3f2fd">
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/products" class="nav-link">products</router-link>
+      <router-link to="/about" class="nav-link">about</router-link>
+      <router-link to="/contact" class="nav-link">contact</router-link>
+    </nav>
+  </div>
+</template>
